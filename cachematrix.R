3402aa6d851a3d0e6e15752bb15695872b71e6d2
zinger111 <zinger111@mail.ru>
2016-10-15 19:39:33 +0300

    Functions work as requested.
    Disclaimer:
    I intentionaly kept my syntrax as C-ish as possible.
    Reason - I'm from C, C++, C# world :)
    As styling rules where not defined, I hope it will not influence evaluation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a set of functions that introduces caching of matrix inverse results.
 
-## Write a short comment describing this function
+## This function allows to create instances of cache-enabled matrices 
+## from common square invertible matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCahceMatrix <- function(original = matrix()) {
+  inverce <- NULL
+  set <- function(value) {
+    original <<- value
+    inverce  <<- NULL
+  }
+  get <- function() {return(original)} 
+  
+  setInverse <- function(solve) {inverce <<- solve}
+  
+  getInverse <- function() {return(inverce)}
+  
+  list(
+    get = get,
+    set = set,
+    getInverse = getInverse,
+    setInverse = setInverse
+  )
 }
 
+## This function can resolve inverse of 'makeCacheMatrix' matrixes utilizing caching functionality of src. object.
+## @cacheableMatrix => expects makeCahceMatrix instance.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cacheableMatrix, ...) {
+  ## Return a matrix that is the inverse of 'cacheableMatrix'
+  inverce <- cacheableMatrix$getInverse()
+  if (is.null(inverce)) {
+    message("calculating invecrce")
+    original <- cacheableMatrix$get()
+    inverce <- solve(original, ...)
+    cacheableMatrix$setInverse(inverce)
+  }
+  else{
+    message("getting cached data")
+  }
+  return(inverce)
 }
